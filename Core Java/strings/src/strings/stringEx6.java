d4f03a431fdcbbc78d2bc6a1346ddcdc4c389b62
AKASH R <56690454+Akash13345526@users.noreply.github.com>
2022-11-28 14:44:15 +0530

    Update stringEx6.java

--- Core Java/strings/src/strings/stringEx6.java
+++ Core Java/strings/src/strings/stringEx6.java
@@ -2,10 +2,10 @@ package strings;
 
 public class stringEx6 {
 	public static void main(String [] args) {
-		String Ex6=new String("bhuva");
-		String Ex61=new String("bhuvanesh");
-		String name="BHUVA";
-		String name2="    bhuvanesh BABU     ";
+		String Ex6=new String("Aka");
+		String Ex61=new String("Akash");
+		String name="AKASH";
+		String name2="    Akash R     ";
 		System.out.println(Ex6==name);
 		System.out.println(Ex6.equals(name));
 		System.out.println(Ex6.equalsIgnoreCase(name));
@@ -19,9 +19,9 @@ public class stringEx6 {
 		
 		
 		System.out.println(name2.trim());
-		System.out.println(name2.replace("BABU", "kumar"));
+		System.out.println(name2.replace("R", "kumar"));
 		//name2.delete(1,3);
-		StringBuffer sb=new StringBuffer("bhuvanesh");
+		StringBuffer sb=new StringBuffer("Akash");
 		//System.out.println(sb.delete(1, 3));
 		//System.out.print(sb.length());
 		
