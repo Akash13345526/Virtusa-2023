9d280d95584512addfbbcb9409ec0b8368182b95
AKASH R <56690454+Akash13345526@users.noreply.github.com>
2022-11-28 14:41:40 +0530

    Update StringEx2.java

--- Core Java/strings/src/strings/StringEx2.java
+++ Core Java/strings/src/strings/StringEx2.java
@@ -4,21 +4,16 @@ public class StringEx2 {
 	
 	public static void main(String[] args) {
 		
-		String S1="bhuvan";
+		String S1="Akash";
 		System.out.println(S1.toString());
 		System.out.println(S1);
 		
-		StringBuffer S2=new StringBuffer("bhuvanes");
+		StringBuffer S2=new StringBuffer("Rajan");
 		System.out.println(S2);
 		System.out.println(S2.toString());
 		
 		objecct obj=new objecct();
 		System.out.println(obj);
-		
-		
-		
-		
-		
 		
 	}
 	
