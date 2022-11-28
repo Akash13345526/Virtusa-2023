aa66f5e310ae899ac19193c611f0077090b39692
AKASH R <56690454+Akash13345526@users.noreply.github.com>
2022-11-28 14:42:23 +0530

    Update StringEx3.java

--- Core Java/strings/src/strings/StringEx3.java
+++ Core Java/strings/src/strings/StringEx3.java
@@ -2,13 +2,13 @@ package strings;
 
 public class StringEx3 {
 	public static void main(String[] args) {
-		String S1="bhuvan";
+		String S1="Akash";
 		StringBuffer str=new StringBuffer(S1);
 		
 		System.out.println(str.reverse());
 		
 		
-		StringBuffer st1=new StringBuffer("bhuvbanbdbd");
+		StringBuffer st1=new StringBuffer("Akakaaash");
 		String s2=st1.toString();
 		System.out.println(s2+st1);
 		String s3=s2+st1;
@@ -19,7 +19,7 @@ public class StringEx3 {
 		int a[]=new int[100];
 		System.out.println(a.length);
 		
-		String s4="bhuvab";
+		String s4="akaaash";
 		System.out.println(s4.length());
 		
 		
